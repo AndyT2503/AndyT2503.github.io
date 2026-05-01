a5b73b04b034253c4cae6b2ecf0eac0fb35f1dea
TuHoang <htu25399@gmail.com>
2026-05-01 10:37:01 +0700

    fix: update script generate sitemap

--- scripts/prerender-seo.js
+++ scripts/prerender-seo.js
@@ -155,12 +155,21 @@ console.log('✅ Blog pages generated');
 
 // ===== Generate sitemap.xml =====
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                      http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
 ${urls.map(url => `
   <url>
     <loc>${url}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${url === 'https://andyt2503.github.io' ? '1.0' : '0.8'}</priority>
   </url>
 `).join('')}
+
 </urlset>`;
 
 fs.writeFileSync(path.join(DIST_PATH, 'sitemap.xml'), sitemap);
