5f5652125c85a7177753faf03c47aafb39729500
TuHoang <htu25399@gmail.com>
2026-05-09 16:23:24 +0700

    chore: remove loading component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,6 @@
-
 import {
   ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  PLATFORM_ID,
-  inject,
-  signal
+  Component
 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -13,12 +8,8 @@ import {
   EmailComponent,
   FooterComponent,
   HeaderComponent,
-  LoadingOpenComponent,
-  SocialComponent,
+  SocialComponent
 } from '../layout';
-import { StorageKey } from '../shared/const';
-import { SessionStorageService } from '../shared/services';
-import { isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-main',
@@ -28,34 +19,11 @@ import { isPlatformServer } from '@angular/common';
     EmailComponent,
     FooterComponent,
     HeaderComponent,
-    LoadingOpenComponent,
     SocialComponent,
-    RouterOutlet
-],
+    RouterOutlet,
+  ],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent implements OnInit {
-  private readonly storage = inject(SessionStorageService);
-  private platformId = inject(PLATFORM_ID);
-  isOpen = signal(false);
-
-  ngOnInit(): void {
-    this.showLoadingAnimation();
-  }
-
-  private showLoadingAnimation() {
-    if (isPlatformServer(this.platformId)) {
-      this.isOpen.set(true);
-      return;
-    }
-    this.isOpen.set(!!this.storage.getItem(StorageKey.wasLoaded));
-    if (!this.isOpen()) {
-      setTimeout(() => {
-        this.isOpen.set(true);
-        this.storage.setItem(StorageKey.wasLoaded, true);
-      }, 4000);
-    }
-  }
-}
+export class MainComponent {}
